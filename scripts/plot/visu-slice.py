34a17c2d6d80608d2297bf69abcc09566e125000
Thomas Ithurbide <thomas.ithurbide.1@etu.u-bordeaux.fr>
2025-12-21 15:20:45 +0100

    Ajout de la possibilité de visualiser tout un folder de snapshots

--- scripts/plot/visu-slice.py
+++ scripts/plot/visu-slice.py
@@ -5,7 +5,7 @@ import re
 import argparse
 import time
 from matplotlib.colors import LinearSegmentedColormap
-
+import os
 
 
 # Script python permettant de visualiser une slice soit:
@@ -14,6 +14,11 @@ from matplotlib.colors import LinearSegmentedColormap
 
 # Fonctionne uniquement avec ex==ey==ez (le but de ce script étant le benchmark, cela simplifie les choses)
 # La dimension fixée est la dimension Z, on visualise le plan X-Y
+
+
+# Si l'input donnée est un dossier, alors on fait la visualisation pour chaque fichier .bin (snapshot) du dossier
+# Sinon, uniquement pour le fichier donné en input.
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--order')
 parser.add_argument('-z', help="The Z fixed coordinate")
@@ -23,6 +28,7 @@ parser.add_argument('--slice-snapshot', help="If present, this flag means the sn
 parser.add_argument('-i', '--input', help="Input path")
 
 args = parser.parse_args()
+
 
 size = int(args.size)
 order = int(args.order)
@@ -34,32 +40,40 @@ startingLine = sizeDim * int(args.z)
 if (args.slice_snapshot):
     startingLine = 0
 
+if os.path.isdir(args.input):
+    files = []
+    for fName in os.listdir(args.input):
+        if fName.endswith('.bin'):
+            files.append(os.path.join(args.input, fName))
+else:
+    files = [args.input]
+
 
 t0 = time.time()
 
 
+for singleFile in files:
+  data = []
+  with open(singleFile, 'r') as f:
+      for i, line in enumerate(f):
+          if i < startingLine:
+              continue
+          if i >= startingLine + sizeDim:
+              break
+          row = [float(x) for x in line.strip().split()]
+          data.append(row)
 
-data = []
-with open(args.input, 'r') as f:
-    for i, line in enumerate(f):
-        if i < startingLine:
-            continue
-        if i >= startingLine + sizeDim:
-            break
-        row = [float(x) for x in line.strip().split()]
-        data.append(row)
+  data_array = np.array(data)
+  plt.figure(figsize=(6, 5))
 
-data_array = np.array(data)
-plt.figure(figsize=(6, 5))
+  colors = [(0, 0, 1), (0, 1, 0)] 
+  cmap = LinearSegmentedColormap.from_list("my_cmap", colors)
+  sns.heatmap(data_array, cmap=cmap)
 
-colors = [(0, 0, 1), (0, 1, 0)] 
-cmap = LinearSegmentedColormap.from_list("my_cmap", colors)
-sns.heatmap(data_array, cmap=cmap)
-
-plt.xlabel("X")
-plt.ylabel("Y")
-outputPath = f"{ (args.input.split('/'))[-1].split('.')[:-1][0]}--visu_slice.png"
-plt.savefig(outputPath)
+  plt.xlabel("X")
+  plt.ylabel("Y")
+  outputPath = f"{ (singleFile.split('/'))[-1].split('.')[:-1][0]}--visu_slice.png"
+  plt.savefig(outputPath)
 
 t1 = time.time()
 
